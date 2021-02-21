7afb9a75f53e8ac41846d7f77167343e35fbb759
feedmex <freddylo_@hotmail.com>
2021-02-20 21:53:20 -0500

    updating app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,6 @@ import {useState} from 'react'
 import { ProgressBar} from 'react-bootstrap'
 import axios from 'axios';
 import Typed from 'react-typed';
-import {Redes} from './Redes';
 
 const API = process.env.REACT_APP_API
 
@@ -63,31 +62,40 @@ export const Contact = () => {
     return (
         <section className="contact container">
             <div className="row">
-                <div className="col-lg-4 col-md-6 mt-4 animate__animated animate__zoomIn">
-                    <div className="row ml-auto">
-                        <i className={`fas fa-map-marker-alt icons text-${color} mt-2`}></i>
+                <div className="col-md-6 mt-4 animate__animated animate__zoomIn">
+                    <div className="row mb-5 ml-1">
+                        <i className={`fas fa-map-marker-alt text-${color} mt-2`}></i>
                         <div className="ml-4">
                             <h5>Ubicación</h5>
                             <p>Cusco - Perú</p>
                         </div>
                     </div>
-            
-                    <div className="row mt-4 mb-4 ml-auto">
-                        <i className={`fas fa-envelope-open-text icons text-${color} mt-2`}></i>
+
+                    <div className="row mb-5 ml-1">
+                        <a href="https://github.com/FreddyLimachi" target="_blank" rel="noopener noreferrer">
+                            <i className={`fab fa-github text-${color} mt-2`}></i>
+                        </a>
                         <div className="ml-4">
-                            <h5>Correo</h5>
-                            <p>freddylo_@hotmail.com</p>
+                            <h5>GitHub</h5>
+                            <a href="https://github.com/FreddyLimachi" target="_blank" rel="noopener noreferrer">
+                                github.com/FreddyLimachi
+                            </a>
                         </div>
                     </div>
             
-                    <div className="row mb-5 ml-auto">
-                        <i className={`fas fa-phone icons text-${color} mt-2`}></i>
+                    <div className="row mb-5 ml-1">
+                        <a href="https://www.linkedin.com/in/freddylimachi" target="_blank" rel="noopener noreferrer">
+                            <i className={`fab fa-linkedin-in ml-1 text-${color} mt-2`}></i>
+                        </a>
                         <div className="ml-4">
-                            <h5>Telefono</h5>
-                            <p>+51 952 684 111</p>
+                            <h5>Linkedin</h5>
+                            <a href="https://www.linkedin.com/in/freddylimachi" target="_blank" rel="noopener noreferrer">
+                                .../in/freddylimachi
+                            </a>
                         </div>
-                    </div>
+                    </div>  
                 </div>
+                
                 <div className="col-md-6 col-lg-6 ml-auto animate__animated animate__zoomIn">
                     <div className="d-flex justify-content-between">
                         <h4>
@@ -98,8 +106,7 @@ export const Contact = () => {
                                 backSpeed={100}
                                 loop
                             ></Typed>
-                        </h4>
-                        <Redes/>
+                        </h4>             
                     </div>
                     
                     <Bar/>   
